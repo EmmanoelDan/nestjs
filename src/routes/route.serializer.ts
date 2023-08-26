73e6194441cccbfc72cd28aa446873ba5c2d0313
Emmanoel Dantas <emmanuel2011.07@gmail.com>
2023-08-26 04:50:59 -0300

    :arrows_counterclockwise: implements atualization routes services, and add route serialize

---
+++ src/routes/route.serializer.ts
@@ -0,0 +1,39 @@
+import { DirectionsResponseData } from "@googlemaps/google-maps-services-js";
+import { Route } from "@prisma/client";
+
+export class RouteSerializer implements Omit<Route, 'directions'> {
+    id: string;
+    name: string;
+    source: {
+        name: string;
+        location: {
+            lat: number;
+            lng: number;
+        }
+    }
+    destination: {
+            name: string;
+            location: {
+                lat: number;
+                lng: number;
+            },
+    }
+    duration: number;
+    distance: number;
+    directions: DirectionsResponseData & {request: any};
+    created_at: Date;
+    updated_at: Date;
+
+    constructor(route: Route) {
+        this.id = route.id;
+        this.name = route.name;
+        this.source = route.source;
+        this.destination = route.destination;
+        this.duration = route.duration;
+        this.distance = route.distance;
+        this.directions = JSON.parse(route.directions as string);
+        this.created_at = route.created_at;
+        this.updated_at = route.updated_at;
+    }
+}
+    
